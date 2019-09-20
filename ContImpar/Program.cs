695376e669180f05b0e70ecd924eec9ef61029e5
Leonardolo <leonardolopezpaz@hotmail.com>
2019-09-20 11:02:29 -0300

    CSharp

---
+++ ContImpar/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ContImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 1; i <= 100; i++){
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
---
+++ Imparpar/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Imparpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N1=0;
+            int resto;
+
+
+            Console.WriteLine("Digite um número:");
+            N1 = int.Parse(Console.ReadLine());
+            
+            resto= N1%2;
+
+            if (resto==0)
+            {
+                Console.WriteLine("O número é par!");
+            }
+            else
+            {
+               Console.WriteLine("O número é impar!");
+            }
+
+        }
+    }
+}
