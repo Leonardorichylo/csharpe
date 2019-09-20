4a0ca7d05989baf60bbc354427e6b48ca55a20f4
Leonardolo <leonardolopezpaz@hotmail.com>
2019-09-20 11:18:07 -0300

    CSharp

---
+++ Tabuada5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Tabuada5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=0;
+            int cont=5;
+
+            for (i=0; i>10; i++)
+            {
+            }
+            
+        }
+    }
+}
