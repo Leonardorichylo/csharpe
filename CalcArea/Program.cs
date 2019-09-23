b7dc635039b1522d91d80b9deda7ce5cacb3c281
Leonardolo <leonardolopezpaz@hotmail.com>
2019-09-23 11:29:30 -0300

    Csharp

---
+++ CalcArea/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CalcArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string forma;
+            do{
+
+            Console.WriteLine("Calcula Area");
+            Console.WriteLine("1 - Quadrado");
+            Console.WriteLine("2 - Triangulo");
+            Console.WriteLine("3 - Círculo");
+            Console.WriteLine("4 - Trapézio");
+            Console.WriteLine("5 - Retângulo");
+            Console.WriteLine("6 - Losango");
+            Console.WriteLine("Fim para terminar");
+            Console.WriteLine("Selecione a forma");
+            forma = Console.ReadLine();
+
+        switch(forma) {
+            case "1":
+            Console.WriteLine("Qual o raio da circunferencia?  ");
+            double r = double.Parse(Console.ReadLine());
+            double area=3.14 * r * r;
+            Console.WriteLine("A area de circunferencia é " + area);
+            break;
+            
+            default:
+                    Console.WriteLine("Operação Invalida!");
+                    break;
+                    }
+        }
+        while(forma)
+        
+    }
+}
---
+++ loginsenha/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace loginsenha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string login, senha;
+
+            Console.Write("Digite seu login: ");
+            login= (Console.ReadLine());
+            Console.Write("Digite sua senha: ");
+            senha= (Console.ReadLine());
+
+            if ((login=="admin") && (senha=="admin")) {
+                Console.WriteLine("Bem vindo Administrador!");
+            }
+            else {
+                Console.WriteLine("Bem vindo usuario!");
+            }
+        }
+    }
+}
