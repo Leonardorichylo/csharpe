26c6913a2fc64827e41a624c5140aeb1de6e960c
Leonardolo <leonardolopezpaz@hotmail.com>
2019-10-10 11:10:53 -0300

    CSharp

--- Tabuada/Program.cs
+++ Tabuada/Program.cs
@@ -1,20 +1,17 @@
 ﻿using System;
 
-namespace Tabuada
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-           for(int i=0; i <=10; i++){
-               for(int j=1; j<=10;j++){
-                   Console.Write($"{j,-2} * {i,-2} = {j*i,-3}  ");
-               }
+namespace Tabuada {
+    class Program {
+        static void Main (string[] args) {
+            for (int i = 0; i <= 10; i++) {
+                for (int j = 1; j <= 10; j++) {
+                    Console.Write ($"{j,-2} * {i,-2} = {j*i,-3}  ");
+                }
 
-               Console.WriteLine();
+                Console.WriteLine ();
 
-           }
-            
+            }
+
         }
     }
 }
