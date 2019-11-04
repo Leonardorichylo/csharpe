8f7be47ed3a8ebbf79108d212091d1b0872e8121
Leonardolo <leonardolopezpaz@hotmail.com>
2019-11-04 12:16:12 -0200

    04/11

---
+++ Kahoot/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Kahoot {
+    class Program {
+        static void Main (string[] args) {
+
+            int[, ] matrix = { 
+                { 10, 15, 48 },
+                { 56, 97, 77 },
+                { 2, 100, 33 }
+            };
+            int soma=0;
+            for(int i=0;i<matrix.GetLength(0);i++){
+                for (int j =0; j<matrix.GetLength(1); j++){
+                    soma=matrix[0];
+                }
+            }
+            System.Console.WriteLine(soma);
+        }
+    }
+}
